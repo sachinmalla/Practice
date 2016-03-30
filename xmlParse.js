ee48a06972823c701e30f3260f8a244e9f1a7d3a
sachinmalla <mallasacheen@gmail.com>
2016-03-29 22:30:20 -0400

    1. parseXML

---
+++ xmlParse.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$.ajax({
+		url:'http://localhost/files/book.xml',
+		dataType:'xml',
+		success:parseXML,
+		error:showErr
+	})
+
+	function parseXML(data){
+		$(data).find('book').each(function(){
+			var name=$(this).find('title').text();
+			var writer=$(this).find('author').text();
+			$('ul').append('<li>'+name+' --> '+writer+'</li>')
+		});
+	};
+
+	function showErr(){
+		alert('test');
+	}
+})
